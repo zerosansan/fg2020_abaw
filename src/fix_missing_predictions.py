18699d8b4949a13211632dfbd60692d5f111ef31
Zero <zero.sanzo@gmail.com>
2020-10-08 17:50:45 +0800

    Fixed missing frames, added checker to check between expected frame number and output frame.

--- src/fix_missing_predictions.py
+++ src/fix_missing_predictions.py
@@ -30,7 +30,68 @@ os.chdir('../corrected')
 
 for i in range(len(corrected_frame_list)):
     f = open(str(prediction_text_file_list[i]), "w+")
-    f.write("Neutral,Anger,Disgust,Fear,Happiness,Sadness,Surprise\n")
     for j in range(len(corrected_frame_list[i])):
         f.write(corrected_frame_list[i][j] + "\n")
     f.close()
+
+os.chdir('../src')
+filename_expected_frame_list = []
+with open("Expr_Challenge_video_and_total_number_of_frames.txt", "r") as fd:
+    for line in fd:
+        line = line.strip()
+        line = line.split()
+        filename = line[2] + ".txt"
+        frame_no = int(line[9])
+        _list = [filename, frame_no]
+        filename_expected_frame_list.append(_list)
+
+os.chdir('../corrected')
+files = os.listdir()
+
+filename_current_frame_list = []
+for i in range(len(files)):
+    count = 0
+    with open(files[i], "r") as fd:
+        for line in fd:
+            count += 1
+
+        filename_current_frame_list.append(count)
+
+for i in range(len(files)):
+    f = open(str(files[i]), "a")
+    for j in range(len(filename_expected_frame_list)):
+        if files[i] == filename_expected_frame_list[j][0]:
+            frame_no = filename_expected_frame_list[j][1]
+            for k in range(frame_no - filename_current_frame_list[i]):
+                f.write("-1" + "\n")
+    f.close()
+
+for i in range(len(files)):
+    f = open(str(files[i]), "r")
+    header_line = "Neutral,Anger,Disgust,Fear,Happiness,Sadness,Surprise" + "\n"
+    lines = f.readlines()
+    lines.insert(0, header_line)
+    f.close()
+
+    # We again open the file in WRITE mode
+    f = open(str(files[i]), "w")
+    f.writelines(lines)
+    f.close()
+
+filename_output_frame_list = []
+for i in range(len(files)):
+    count = 0
+    with open(files[i], "r") as fd:
+        for line in fd:
+            count += 1
+
+        _list = [files[i], count - 1] # disregard header line
+    filename_output_frame_list.append(_list)
+
+sorted_filename_output_frame_list = sorted(filename_output_frame_list)
+sorted_filename_expected_frame_list = sorted(filename_expected_frame_list)
+
+print(sorted_filename_output_frame_list)
+print(sorted_filename_expected_frame_list)
+
+print(sorted_filename_output_frame_list == sorted_filename_expected_frame_list)
